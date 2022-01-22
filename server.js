00e24cd36c65aa159bdb6920787be2636564e2e6
Nosa Obaseki <nosaobaseki@nosa.lan>
2022-01-22 23:46:32 +0100

    Content security policy

--- server.js
+++ server.js
@@ -7,6 +7,16 @@ var app = express()
 // Middleware
 app.use(helmet())
 app.use(helmet.frameguard({ action: 'sameorigin' }))
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            "script-src": ["'self'", "nosaobaseki.com", "c0depanda.com"],
+            "img-src": ["'self'", "nosaobaseki.com", "c0depanda.com"],
+            "style-src": ["'self'", "nosaobaseki.com", "c0depanda.com", "fonts.googleapis.com"],
+            "font-src": ["'self'", "nosaobaseki.com", "c0depanda.com", "fonts.googleapis.com"],
+        },
+    })
+);
 app.use("/", serveStatic(path.join(__dirname, '/dist')))
 
 // Catch all routes and redirect to index
